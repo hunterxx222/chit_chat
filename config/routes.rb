1ff871ed21d4be0be531036f072ebe441fc08e0d
Toan Ngo <xhunter9x@gmail.com>
2016-10-20 12:03:05 +0700

    nearly done required function 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'login' => "sessions#new"
+  resources :sessions, only: [:new, :create]
+
+  delete 'logout' => 'sessions#destroy'
   resources :users
   root 'home#index'
 
