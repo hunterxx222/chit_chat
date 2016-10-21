97889230ebc4227d546983974770c1dde5bb1d90
Toan Ngo <xhunter9x@gmail.com>
2016-10-21 10:58:47 +0700

    done messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :name, :email, presence: true
+  validates :email, uniqueness: {case_sensitive: false}
+
   has_secure_password
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020171353) do
+ActiveRecord::Schema.define(version: 20161021030750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20161020171353) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "name"
   end
 
 end
